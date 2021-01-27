53bff9dd0d0572637154925b25ce8a7d626d0388
Mathis Rousseau <mathisr@outlook.fr>
2021-01-27 20:24:27 +0100

    Last Changements

--- TP2_Progra3D/Assets/EnnemySpawn.cs
+++ TP2_Progra3D/Assets/EnnemySpawn.cs
@@ -6,28 +6,36 @@ using System;
 public class EnnemySpawn : MonoBehaviour
 {
     
-    int starting_ennemies = 3; 
+    int ennemies = 3;
     int enemyname = 0;
-    int max_ennemies = 10;
+    public List<GameObject> enemyList = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < starting_ennemies; i++){
-            Spawn();
-            ennemies+=1;
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float a = UnityEngine.Random.Range(-4f,2f);
-        float b = UnityEngine.Random.Range(2.2f,5f);
-        float c = UnityEngine.Random.Range(10f,15f);
-        GameObject Enemy = (GameObject) Instantiate(Resources.Load("Ennemy"), new Vector3(a,b,c), Quaternion.Euler(0, -90,0));
-        Enemy.name = "Enemy" + enemyname;
-        enemyname ++;
+        while (enemyList.Count < ennemies){
+            float a = UnityEngine.Random.Range(-4f,2f);
+            float b = UnityEngine.Random.Range(2.5f,5f);
+            float c = UnityEngine.Random.Range(10f,15f);
+            GameObject Enemy = (GameObject) Instantiate(Resources.Load("Ennemy"), new Vector3(a,b,c), Quaternion.Euler(0, -90,0));
+            Enemy.name = "Enemy" + enemyname;
+            enemyname ++;
+            enemyList.Add(Enemy);
+        }   
         
+        for (int i = 0; i < enemyList.Count; i++)
+        {   
+            if (enemyList[i] == null){
+                float a = UnityEngine.Random.Range(-4f,2f);
+                float b = UnityEngine.Random.Range(2.5f,5f);
+                float c = UnityEngine.Random.Range(10f,15f);
+                enemyList[i] = (GameObject) Instantiate(Resources.Load("Ennemy"), new Vector3(a,b,c), Quaternion.Euler(0, -90,0));
+            }   
+        }
     }
-
 }
