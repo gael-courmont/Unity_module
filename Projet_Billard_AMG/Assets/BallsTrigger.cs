2727f49af5979e41c9e657b8d7a24815b472ec56
AdelieVsr <65895159+AdelieVsr@users.noreply.github.com>
2021-02-24 14:12:37 +0100

    Update Trigger

--- Projet_Billard_AMG/Assets/BallsTrigger.cs
+++ Projet_Billard_AMG/Assets/BallsTrigger.cs
@@ -1,22 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class BallsTrigger : MonoBehaviour
 {
+    
+    private int Score;
+    [SerializeField] private TextMeshProUGUI scoretext;
+    
+    void Start()
+    {
+        Score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoretext.text = "Score: "+Score;
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         //Trigger fallen ball and its number
         Debug.Log("fallen ball : ");
         Debug.Log(other.gameObject.name);
-        
-        //Increment score in UI ***TO DO***
-        //GameObject ScoreCount = GameObject.Find("Text") as GameObject;
-        //ScoreCount.SendMessageUpwards("IncrementScore");
-        
+
         //Destroy fallen ball and add it in score UI ***TO DO***
         Destroy(other.gameObject);
         
-        //Respawn white and dark balls ***TO DO***
+        //Respawn white ball
+        if (other.gameObject.name == "BallWhite")
+        {
+            //WhiteSpawn.SendMessageUpwards("Start");
+        }
+        else
+        {
+            IncrementScore();
+        }
+        
+    }
+
+    public void IncrementScore()
+    {
+        Score = Score + 1;
     }
 }
