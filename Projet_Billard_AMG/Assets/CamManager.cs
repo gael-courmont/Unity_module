c07710dac2ade0cd08f6bcba3e3bec21afb6c715
Mathis Rousseau <mathisr@outlook.fr>
2021-02-24 11:31:33 +0100

    SpawnBallPasOuf

---
+++ Projet_Billard_AMG/Assets/CamManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamManager : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+    public GameObject cam3;
+    public GameObject cam4;
+    public GameObject cam5;
+    public GameObject cam6;
+    public GameObject cam7;
+    public GameObject cam8;
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1)){
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);  
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2)){
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3)){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(true);
+            cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);   
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad4)){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(true);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);   
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad5)){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(true);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(false);   
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad6)){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(true);
+            cam7.SetActive(false);
+            cam8.SetActive(false);   
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad7)){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(true);
+            cam8.SetActive(false);   
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad8)){
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam4.SetActive(false);
+            cam5.SetActive(false);
+            cam6.SetActive(false);
+            cam7.SetActive(false);
+            cam8.SetActive(true);   
+        }
+
+    }
+}
